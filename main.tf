ec43da9af79925351d641af0b60248fbbe3993ce
Divya Shukla <divyashukla20993@gmail.com>
2025-10-17 20:40:37 +0530

    feat: test

--- main.tf
+++ main.tf
@@ -13,57 +13,57 @@ provider "aws" {
 #     values = [data.aws_vpc.default.id]
 #   }
 # }
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "my-vpc"
-  }
-}
+# resource "aws_vpc" "main" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
+#   tags = {
+#     Name = "my-vpc"
+#   }
+# }
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+# resource "aws_subnet" "public" {
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = "10.0.1.0/24"
+#   map_public_ip_on_launch = true
+#   availability_zone       = "us-east-1a"
 
-  tags = {
-    Name = "public-subnet"
-  }
-}
+#   tags = {
+#     Name = "public-subnet"
+#   }
+# }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_internet_gateway" "gw" {
+#   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "main-igw"
-  }
-}
+#   tags = {
+#     Name = "main-igw"
+#   }
+# }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
 
-  tags = {
-    Name = "public-rt"
-  }
-}
+#   tags = {
+#     Name = "public-rt"
+#   }
+# }
 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
+# resource "aws_route_table_association" "public_assoc" {
+#   subnet_id      = aws_subnet.public.id
+#   route_table_id = aws_route_table.public.id
+# }
 
 
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
   description = "Allow HTTP and SSH traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = var.vpc_id
 
   ingress {
     description = "Allow HTTP"
@@ -99,7 +99,10 @@ resource "aws_instance" "web" {
   subnet_id                    = var.subnet_id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
-
+    
+  provisioner "local-exec" {
+    command = "echo EC2 instance created >> instance_log.txt"
+  }
 
   tags = {
     Name = "Web-Server"
