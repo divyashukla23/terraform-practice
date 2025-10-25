7627ffaa7966927b6ea874f7cff9501da029ceee
Divya Shukla <divyashukla20993@gmail.com>
2025-10-25 11:55:22 +0530

    feat: test

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id = aws_vpc.main_vpc
+  vpc_id = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone = "ap-south-1a"
