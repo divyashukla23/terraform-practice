a261450f566599cde2d5b39c04bcc5034a1af26a
Divya Shukla <divyashukla20993@gmail.com>
2025-10-25 11:12:09 +0530

    feat: test

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "The ID of the insatnec being created"
-  value = aws_instance.web.id
-}
-
-output "instance_public_ip" {
-  description = "the public IP of my ec2"
-  value = aws_instance.web.public_ip
-}
