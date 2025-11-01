9e1e1d381200cc8bcbf52262d756060283609e71
Divya Shukla <divyashukla20993@gmail.com>
2025-11-01 12:24:06 +0530

    feat: test

--- terraform-workspaces-demo/main.tf
+++ terraform-workspaces-demo/main.tf
@@ -15,3 +15,19 @@ output "bucket_name" {
   description = "S3 bucket name created for this workspace"
   value       = aws_s3_bucket.demo.bucket
 }
+# another way of passing creds in tf sensitivly :
+# aws ssm put-parameter \                                                             ─╯
+#   --name "db_password" \
+#   --value "MySecurePass123" \
+#   --type "SecureString"
+
+# refernce it in tf using : 
+# data "aws_ssm_parameter" "db_password" {
+#   name = "db_password"
+#   with_decryption = true
+# }
+
+# output "password" {
+#   value     = data.aws_ssm_parameter.db_password.value
+#   sensitive = true
+# }
